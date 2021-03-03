d6471a64aab4d57104bd095b3d55c0febe88638d
DannyNoe <dannymnoe@gmail.com>
2021-03-02 20:09:56 -0500

    Added eval, cx, and mut functions

--- gen_alg.py
+++ gen_alg.py
@@ -16,6 +16,29 @@ import ctcsound
 
 from Representation import Melody
 
+POP_SIZE = 6    # Dictates the number of melodies in a population
+
+def evaluate_music(input_mel):
+    """
+    Evaluation function for music that evaluates a given melody
+    """
+    print(str(input_mel))
+    #play(melody) # Need function to play each melody for user
+    usr_rating = input("Did you enjoy that little diddy? Please rate from 1-5 (ints only) ")
+    return int(usr_rating),
+
+def cx_music(input_mel1, input_mel2):
+    """
+    Crossover function for music that performs a crossover operation on two given melodies: input_mel1, input_mel2
+    """
+    return
+
+def mut_music(input_mel):
+    """
+    Mutation function for music that mutates a give melody: input_mel
+    """
+    return
+
 test_mel = Melody()
 print(test_mel)
 print(test_mel.len())
@@ -27,10 +50,13 @@ creator.create("Melody", Melody, fitness=creator.FitnessMax)
 toolbox = base.Toolbox()
 toolbox.register("population", tools.initRepeat, list, creator.Melody)
 
-the_populatation = toolbox.population(n=2)
+the_populatation = toolbox.population(n=POP_SIZE)
 print(the_populatation)
 
 i = 0
 for mel in the_populatation:
-    print("Melody " + str(i) + str(mel))
+    #print("Melody " + str(i) + " " + str(mel))
     i+=1
+
+for mel in the_populatation:
+    print(evaluate_music(mel))
