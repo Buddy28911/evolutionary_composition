17a6a7b8af65850204c4a1cf6f0500193f961b66
DannyNoe <dannymnoe@gmail.com>
2021-04-18 14:37:13 -0400

    Continued work on command line args

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 # Main file for Danny Noe's Evolutionary Music Project
 
-from music_data import *
+#from music_data import *
+import argparse
 
-def set_key_signature():
-    print(KEY)
-    print("The default key signature is ")
-    # yes_no = input("Would you like to change the key signature?")
-    # yes_no = yes_no.lower()
-    key_change = False
+key_sig = ["Cb","Gb","Db","Ab","Eb","Bb","F","C","G","D","A","E","B","F#","C#"]
+parser = argparse.ArgumentParser()
+#parser.add_argument('-o', '--output', action='store_true', help="shows output")
+parser.add_argument('-v', '--verbosity', action='store_true', help="Outputs program's settings")
+parser.add_argument('-k', '--key_signature', type=str, help="Sets the key signature for the program", choices=key_sig, default="C")
+parser.add_argument('-t', '--tempo', type=int, help="Sets the tempo (in BPM) for the program", choices=range(1, 301), metavar="[0,300]", default=120)
 
-    while True:
-        yes_no = input("Would you like to change the key signature?")
-        yes_no = yes_no.lower()
-        if yes_no == "yes" or yes_no == "y" or yes_no == "no" or yes_no == "n":
-            if yes_no == "yes" or yes_no == "y":
-                key_change = True
-            break
-        else:
-            print("Unknown response")
 
-    if key_change:
-        print("Available key signatures:")
-        print(SCALES.keys())
-        while True:
-            new_key = input("Desired key signature?")
-            if new_key in SCALES.keys():
-                #KEY = new_key
-                break
-            else:
-                print(new_key, " unsupported.")
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+parser.add_argument('-b', '--back_track', type=str2bool, nargs='?', help="Enables a backing track. Turn on the track with True, False for off.", default=True)
+parser.add_argument('-a', '--arp_or_scale', type=str2bool, nargs='?', help="Sets the backing track to play an ascending arpeggio or scale. True for arp, false for scale", default=True)
+# Todo: Add descending option?
 
 def main():
     print("Welcome to the evolutionary composition program!")
-    set_key_signature()
-    print(KEY)
-
+    args = parser.parse_args()
+    if args.verbosity:
+        print("Selected key is", args.key_signature)
+        print("The tempo is", args.tempo)
+        if args.back_track:
+            print("The backing track is on.", end=" ")
+            if args.arp_or_scale:
+                print("Arpeggio selected.")
+            else:
+                print("Scale selected.")
+        else:
+            print("Backing track disabled.")
+        
+    
 
 main()
