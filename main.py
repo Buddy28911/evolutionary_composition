6b770481c4a08ac3db67128b47b25c92a8713a2d
DannyNoe <dannymnoe@gmail.com>
2021-05-05 11:42:09 -0400

    Tweaked default args, added .gitignore to repo

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ key_sig = ["Cb","Gb","Db","Ab","Eb","Bb","F","C","G","D","A","E","B","F#","C#"]
 parser = argparse.ArgumentParser()
 parser.add_argument('-o', '--outport', type=str, help="Sets the outport device for MIDO. If none is given, you will get the (system specific) default port.")
 parser.add_argument('-v', '--verbosity', action='store_true', help="Outputs program's settings")
-parser.add_argument('-k', '--key_signature', type=str, help="Sets the key signature for the program", choices=key_sig, default="C")
+parser.add_argument('-k', '--key_signature', type=str, help="Sets the key signature for the program", choices=key_sig, default="Eb")
 parser.add_argument('-t', '--tempo', type=int, help="Sets the tempo (in BPM) for the program", choices=range(1, 301), metavar="[0,300]", default=120)
 
 def str2bool(v):
@@ -39,11 +39,11 @@ gen_al_args = parser.add_argument_group('Genetic Algorithm Arguments',
                     'eaMuPlusLambda arguments: mu, lambda_, cxpb, mutpb, ngen | eaMuCommaLambda arguments: mu, lambda_, cxpb, mutpb, ngen | eaSimple arguments: cxpb, mutpb, ngen')
 
 gen_al_args.add_argument('--popsize', type=int, help="Sets the number of melodies to generate in the initial population.", default=6)
-gen_al_args.add_argument('--ngen', type=int, help="ngen sets the number of generations the GA will run for.", default=6)
+gen_al_args.add_argument('--ngen', type=int, help="ngen sets the number of generations the GA will run for.", default=2)
 gen_al_args.add_argument('--mu', type=int, help="mu sets the number of individuals to select for the next generation.", default=3)
-gen_al_args.add_argument('--lambda_', type=int, help="lambda_ sets number of children to produce at each generation.", default=10)
-gen_al_args.add_argument('--cxpb', type=float, help="cxpb sets the probability that an offspring is produced by crossover.", metavar="[0,1)", default=0.7)
-gen_al_args.add_argument('--mutpb', type=float, help="mutb sets the probability that an offspring is produced by mutation.", metavar="[0,1)", default=0.3)
+gen_al_args.add_argument('--lambda_', type=int, help="lambda_ sets number of children to produce at each generation.", default=6)
+gen_al_args.add_argument('--cxpb', type=float, help="cxpb sets the probability that an offspring is produced by crossover.", metavar="[0,1)", default=0.3)
+gen_al_args.add_argument('--mutpb', type=float, help="mutb sets the probability that an offspring is produced by mutation.", metavar="[0,1)", default=0.7)
 
 def main():
     print("Welcome to the evolutionary composition program!")
